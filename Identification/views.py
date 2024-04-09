63e6beacfc5f1bab7febbe2604f12db1f9e41b7f
ReverendSafari <s.swanson24@ncf.edu>
2024-04-08 22:32:36 -0400

    Finalizing Both Pages
    Made significant styling overhauls to both pages, as well as fixed some small bugs
    
    ID page
    Added dynamic title, sorting, and non persistent page state (had a token hiding in their)
    
    Education
    Fixed component bug, had an unnecessary script tag in their and some duplicate id's
    Added background pic as well

--- Identification/views.py
+++ Identification/views.py
@@ -1,17 +1,44 @@
-from django.shortcuts import render, redirect
+from Identification.models import State, SnakeSpecies
+from django.shortcuts import render
+
+
+from django.shortcuts import render
 from .models import State, SnakeSpecies
 
 def select_state(request):
+    selected_state_name = "the USA"
+    species = SnakeSpecies.objects.all()  # Default species list for no state selected
+
+    selected_state_id = request.GET.get('state')  # Use GET instead of POST
+    if selected_state_id:
+        try:
+            selected_state = State.objects.get(id=selected_state_id)
+            selected_state_name = selected_state.name
+            species = SnakeSpecies.objects.filter(states__id=selected_state_id)
+        except State.DoesNotExist:
+            # Handle the case where the state does not exist
+            species = SnakeSpecies.objects.all()
+
     states = State.objects.all()
-    species = []
-    if request.method == 'POST':
-        selected_state_id = request.POST.get('state')
-        species = SnakeSpecies.objects.filter(states__id=selected_state_id)
-    
-    return render(request, 'id.html', {
+
+    species_data = []
+    for s in species:
+        species_data.append({
+            'common_name': s.common_name,
+            'scientific_name': s.scientific_name,
+            'description': s.description,
+            'venomous': s.venomous,
+            'image_path': s.image_path,
+        })
+
+    context = {
         'states': states,
-        'species': species,
-    })
+        'species': species_data,
+        'selected_state_name': selected_state_name,
+    }
+
+    return render(request, 'id.html', context)
+
 
 def render_education(request):
     return render(request, 'education.html')
