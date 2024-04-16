de508b1d799b0e3818cb9f5c361849111090e6a3
ReverendSafari <s.swanson24@ncf.edu>
2024-04-15 20:38:06 -0400

    Added my Ip to localhost
    See title, was getting a weird error

--- SerpentSpotProject/settings.py
+++ SerpentSpotProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d98t81%j*)_)_u65m_n07ofnkrq%e3_+4j%z!ybv$f0wvjcqnb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["alien-loving-deadly.ngrok-free.app", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1","localhost", "alien-loving-deadly.ngrok-free.app"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://alien-loving-deadly.ngrok-free.app',
