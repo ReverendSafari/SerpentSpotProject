9a5df116dd17dc53cfea62b63b83262e7e802da7
Schweem <63567335+Schweem@users.noreply.github.com>
2024-04-17 19:19:45 -0400

    Styling and profile improvements
    + Minor styling tweaks to forum and boards
    + linked threads to user profile
    + different user observation journals
    + more dynamic buttons

--- ObservationJournal/urls.py
+++ ObservationJournal/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.journal_view, name='journal_view'),
+    path('<str:username>/', views.journal_view, name='user_journal_view'),
     path('delete/<int:observation_id>/', views.delete_observation, name='delete_observation'),
 ]
