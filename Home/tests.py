faa55e44e7aa4ea587cc20be7208ffb2e78aedf4
Schweem <63567335+Schweem@users.noreply.github.com>
2024-04-24 20:33:06 -0400

    Unit testing
    + form, view and model tests for Home, Leaderboard and Observation Journal apps

--- Home/tests.py
+++ Home/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeViewTests(TestCase):
+    def test_home_view_status_code(self):
+        """
+        Test the status code of the home view.
+
+        This method sends a GET request to the 'home_view' URL and checks if the response
+        status code is equal to 200 (HTTP OK). This ensures that the home view is accessible
+        and returns a successful response.
+
+        """
+        response = self.client.get(reverse('home_view'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_view_uses_correct_template(self):
+        """
+        Test that the home view uses the correct template.
+        """
+        response = self.client.get(reverse('home_view'))
+        self.assertTemplateUsed(response, 'homepage.html')
