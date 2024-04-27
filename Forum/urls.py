d01e97b88b88fc8b394b4a87c1d694fbb6158f30
ReverendSafari <s.swanson24@ncf.edu>
2024-04-27 01:52:28 -0400

    Added Edit and Delete Functionality for Threads/Replies, and Add more form validation
    There is now edit and delete buttons, that open modals for editing and deleting threads or replies to threads
    
    Still need some light styling with placement
    
    I noticed that none of the forms, editing or creating were checking for JUST whitespace so I added basic js validation so the user can't enter just whitespace - Just a JS alert for rn but it works.

--- Forum/urls.py
+++ Forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, board_threads, thread_posts, new_thread, reply_thread, user_profile, delete_thread
+from .views import home, board_threads, thread_posts, new_thread, reply_thread, user_profile, delete_thread, delete_post, edit_post, edit_thread
 
 urlpatterns = [
     path('', home, name='home'),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('thread/<int:thread_id>/reply/', reply_thread, name='reply_thread'),
     path('user/<str:username>/', user_profile, name='user_profile'),
     path('thread/<int:thread_id>/delete/', delete_thread, name='delete_thread'),
+    path('thread/<int:thread_id>/post/<int:post_id>/delete/', delete_post, name='delete_post'),
+    path('thread/<int:thread_id>/edit/', edit_thread, name='edit_thread'),
+    path('post/<int:post_id>/edit/', edit_post, name='edit_post'),
 ]
