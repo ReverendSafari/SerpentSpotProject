1bdab249b511846790c3faaf1c1c9c74a967b6b4
ReverendSafari <s.swanson24@ncf.edu>
2024-04-29 15:42:33 -0400

    Merging back to my branch I totally forgot I was on main

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,11 @@
+# models.py
+
 from django.db import models
 
-# Create your models here.
+class FAQ(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)  # For URL-friendly titles
+
+    def __str__(self):
+        return self.question
--- faq/views.py
+++ faq/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import FAQ  # Make sure to import your FAQ model
 
-# Create your views here.
-def faq_view(request):
-   return render(request, 'faq.html')
+# This view will handle the display of the FAQ list on the home page
+def faq_home(request):
+    faqs = FAQ.objects.all()  # Retrieve all FAQ objects from the database
+    return render(request, 'faq_home.html', {'faqs': faqs})
+
+# This view will handle the display of a specific FAQ based on the slug
+def faq_detail(request, slug):
+    faq = get_object_or_404(FAQ, slug=slug)  # Retrieve the specific FAQ object or return a 404 error
+    return render(request, 'faq_detail.html', {'faq': faq})
