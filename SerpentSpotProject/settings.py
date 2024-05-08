7b16f7aded4f387955ec363f5d4dcbe4ec690feb
ReverendSafari <s.swanson24@ncf.edu>
2024-05-08 13:51:43 -0400

    Fixing image issue, changing debug back on to test

--- SerpentSpotProject/settings.py
+++ SerpentSpotProject/settings.py
@@ -46,7 +46,7 @@ DATABASES = {
 SECRET_KEY = 'django-insecure-d98t81%j*)_)_u65m_n07ofnkrq%e3_+4j%z!ybv$f0wvjcqnb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1","localhost", "alien-loving-deadly.ngrok-free.app", '.ngrok.io', '.elasticbeanstalk.com','serpentspot.applikuapp.com']
 
